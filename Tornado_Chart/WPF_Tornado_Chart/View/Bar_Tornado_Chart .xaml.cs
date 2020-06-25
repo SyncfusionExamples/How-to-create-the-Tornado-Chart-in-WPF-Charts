b608817a8526598cbb8ada5db2109c849974ff33
ramyaprakash17 <65007314+ramyaprakash17@users.noreply.github.com>
2020-06-25 14:09:31 +0530

    Add files via upload
    Added sample Tornado chart

---
+++ Tornado_Chart/WPF_Tornado_Chart/View/Bar_Tornado_Chart .xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Tornado_Chart.View
+{
+    /// <summary>
+    /// Interaction logic for Bar_Tornado_Chart.xaml
+    /// </summary>
+    public partial class Bar_Tornado_Chart : Page
+    {
+        public Bar_Tornado_Chart()
+        {
+            InitializeComponent();
+        }
+    }
+}
