6283f13919a9af77f51bcd0f655679f563aabb8e
subash_s <subash.s@syncfusion.com>
2025-05-16 22:40:17 +0530

    sample update

---
+++ WPF_Tornado_Chart/Model/DepartmentModel.cs
@@ -0,0 +1,37 @@
+﻿namespace WPF_Tornado_Chart.Model
+{
+    public class DepartmentModel
+    {
+        public DepartmentModel(string deptName, double passStart, double pass, double passEnd)
+        {
+            DeptName = deptName;
+            PassStart = passStart;
+            Pass = pass;
+            PassEnd = passEnd;
+        }
+
+        public string DeptName
+        {
+            get;
+            set;
+        }
+
+        public double PassStart
+        {
+            get;
+            set;
+        }
+
+        public double Pass
+        {
+            get;
+            set;
+        }
+
+        public double PassEnd
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ WPF_Tornado_Chart/View/BarTornadoChart.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+namespace WPF_Tornado_Chart.View
+{
+    /// <summary>
+    /// Interaction logic for BarTornadoChart.xaml
+    /// </summary>
+    public partial class BarTornadoChart : UserControl
+    {
+        public BarTornadoChart()
+        {
+            InitializeComponent();
+        }
+    }
+}
